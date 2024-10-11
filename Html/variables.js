d918ea5673024dc7a3591133396fd5c25a9cd678
Michael Ullman <mist83@gmail.com>
2024-10-10 21:54:19 -0600

    cleanup

--- Html/variables.js
+++
@@ -1,9 +0,0 @@
-﻿console.log("Loaded variables.js (hook for finding console debugging)");
-
-let demoMode = false;
-
-const localStorageSettingsKey = "iter9/settings";
-const localStorageDataKey = "iter9/data";
-
-let url = 'https://localhost:7166/api/iter9/snapshot';
-url = "https://zo2lwmn5a2tpg4mmuvhurdrzqu0dzyos.lambda-url.us-west-2.on.aws/api/iter9/snapshot";
